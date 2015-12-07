4c67e71b59e85e57e4ffc5b1ce2c40f67106d519
Ralph <ralph@huwiler.in>
2015-12-07 18:15:14 +0100

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,6 @@
 var gulp = require("gulp");
 var shell = require('gulp-shell');
 var elixir = require('laravel-elixir');
-var themeInfo = require('./theme.json');
-
-var Task = elixir.Task;
-
-elixir.extend("stylistPublish", function() {
-
-    new Task("stylistPublish", function() {
-        gulp.src('').pipe(shell('php ../../artisan stylist:publish ' + themeInfo.name));
-    })
-
-    .watch('resources/**/*.less')
-    .watch('resources/**/*.js');
-});
-
 
 /*
  |--------------------------------------------------------------------------
@@ -42,12 +28,4 @@ elixir(function (mix) {
      * Concat scripts
      */
     mix.browserify("app.js");
-
-    mix.stylistPublish();
-
-    mix.browserSync({
-        proxy: "societycms.dev",
-        port:   8080
-    });
-
 });
